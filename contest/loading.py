8c62e7c77d2a83b98fad7e928bbbf2b71979054c
KooMinJeong <cookie00421@gmail.com>
2023-02-23 16:08:35 +0900

    fix: loading.py 무한루프 버그 수정

--- contest/loading.py
+++ contest/loading.py
@@ -7,9 +7,9 @@ n = int(input())
 print("loding starts\n")
 
 elapsed = 0
-while n > 0:
+while elapsed < n:
     elapsed += 1
     time.sleep(1)
     print("loading... " + str(elapsed))
-    
+
 print("\nloading ends")
